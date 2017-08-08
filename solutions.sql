5752b2261c73a091ae3d2dd2ed07dd3b1e09cde9
tritland <t_ritland@yahoo.com>
2017-08-08 14:44:12 -0500

    Update solutions.sql

--- solutions.sql
+++ solutions.sql
@@ -2,11 +2,11 @@ Get all the users from Chicago
 SELECT * FROM syntax_practice WHERE ILIKE 'Chicago';
 
 Get all users with usernames that contain the letter a.
-SELECT * FROM syntax_practice WHERE username LIKE '%a%'
+SELECT * FROM syntax_practice WHERE username ILIKE '%a%'
 
 The bank is giving a new customer bonus! Update all records with an 
 account balance of 0.00 and a transactions_attempted of 0. Give them a new account balance of 10.00.
-UPDATE syntax_practice SET account_balance = '10.00' WHERE account_balance ='0.00' and transactions_attempted ='0';
+UPDATE syntax_practice SET account_balance = '10.00' WHERE account_balance ='0.00' AND transactions_attempted ='0';
 
 Select all users that have attempted 9 or more transactions.
 SELECT * FROM syntax_practice WHERE transactions_attempted >= '9';
@@ -16,10 +16,10 @@ lowest balance. NOTE: Research LIMIT
 SELECT username, account_balance FROM syntax_practice ORDER BY account_balance DESC LIMIT 3;
 
 Get the username and account balance of the 3 users with the lowest balances, sort lowest to highest balance.
-SELECT username, account_balance FROM syntax_practice ORDER BY account_balance DESC LIMIT 3;
+SELECT username, account_balance FROM syntax_practice ORDER BY account_balance ASC LIMIT 3;
 
 Get all users with account balances that are more than $100.
-SELECT username FROM syntax_practice WHERE account_balance > '100';
+SELECT * FROM syntax_practice WHERE account_balance > '100';
 
 Add a new record.
 INSERT INTO syntax_practice (username, city, transactions_completed, transactions_attempted, account_balance)
